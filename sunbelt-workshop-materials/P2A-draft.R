eebc202d77485f7f9206858a93933ac77053a6cf
martinaboschi <123976778+martinaboschi@users.noreply.github.com>
2026-06-03 12:15:33 +0200

    Update P2A-draft.R

--- sunbelt-workshop-materials/P2A-draft.R
+++ sunbelt-workshop-materials/P2A-draft.R
@@ -15,7 +15,7 @@ kable(tab1, format = "latex", booktabs = TRUE)
 
 # P2.2: Defining the risk set####
 # PROPOSAL
-# ncc_data <- sample_non_events(event_log,
+# raw_data_m1 <- sample_non_events(event_log,
 #                               scope      = "all", 
 #                               # exclude if it has already occurred.
 #                               risk       = "remove",
@@ -28,3 +28,28 @@ kable(tab1, format = "latex", booktabs = TRUE)
 #                                   is_concurrent <- any(fr$time == time)
 #                                   is_native || is_concurrent})
 
+# currently possible in amore (but wrong conceptually for this application):
+raw_data_m1 <- sample_non_events(event_log,
+                              scope      = "all", 
+                              risk       = "remove")
+head(raw_data_m1)
+
+cases_direct <- raw_data_m1[raw_data_m1$event == 1L, ]
+ctrls_direct <- raw_data_m1[raw_data_m1$event == 0L, ]
+cases_direct <- cases_direct[order(cases_direct$stratum), ]
+ctrls_direct <- ctrls_direct[order(ctrls_direct$stratum), ]
+ncc_data  <- data.frame(
+  stratum = cases_direct$stratum,
+  sender_ev = cases_direct$sender,
+  receiver_ev = cases_direct$receiver,
+  sender_nv = ctrls_direct$sender,
+  receiver_nv = ctrls_direct$receiver,
+  d_reciprocity_count = cases_direct$reciprocity_count -
+    ctrls_direct$reciprocity_count,
+  one = 1
+)
+fit_glm <- glm(one ~ d_reciprocity_count - 1,
+               family = "binomial", data = ncc_data)
+tab3 <- ncc_data[ncc_data$stratum<=8,]
+kable(tab3, format = "latex", booktabs = TRUE)
+AIC(fit_glm)
