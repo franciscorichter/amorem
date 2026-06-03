c31cb171f5e3778fc8a08793b93edf3969cd1a55
martinaboschi <123976778+martinaboschi@users.noreply.github.com>
2026-06-03 12:06:54 +0200

    Update P2A-draft.R

--- sunbelt-workshop-materials/P2A-draft.R
+++ sunbelt-workshop-materials/P2A-draft.R
@@ -11,3 +11,20 @@ event_log <- standardize_event_log(first_records[,c(1,3:4)],
 event_log <- event_log[order(event_log$time), ]
 tab1 <- head(event_log)
 kable(tab1, format = "latex", booktabs = TRUE)
+
+
+# P2.2: Defining the risk set####
+# PROPOSAL
+# ncc_data <- sample_non_events(event_log,
+#                               scope      = "all", 
+#                               # exclude if it has already occurred.
+#                               risk       = "remove",
+#                               exclude_fn = 
+#                                 function(sender, receiver, time, fr) {
+#                                   # exclude if is part of the native range
+#                                   is_native     <- any(native$species == sender & 
+#                                                        native$region == receiver)
+#                                   # exclude if it has occurred at the same time
+#                                   is_concurrent <- any(fr$time == time)
+#                                   is_native || is_concurrent})
+
